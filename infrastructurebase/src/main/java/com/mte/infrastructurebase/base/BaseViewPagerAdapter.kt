a238b3ba8077ed4796ef104eb0f2e786934e2ad2
Karima <kmohamed@tacme.com>
2020-01-19 12:46:31 +0200

    feat/create BaseViewPagerAdapter

--- infrastructurebase/src/main/java/com/mte/infrastructurebase/base/BaseViewPagerAdapter.kt
+++ infrastructurebase/src/main/java/com/mte/infrastructurebase/base/BaseViewPagerAdapter.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import java.util.ArrayList
 
-class BaseViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+open class BaseViewPagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager, FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
     private val mFragmentList = ArrayList<Fragment>()
 
     override fun getItem(position: Int): Fragment = mFragmentList[position]
