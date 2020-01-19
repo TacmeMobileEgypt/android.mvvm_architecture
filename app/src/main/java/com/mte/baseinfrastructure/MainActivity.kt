555f5d91a92c97295434282279cf27d528127514
hsaad <hsaad@tacme.com>
2020-01-19 15:18:55 +0200

    commit for changes

--- app/src/main/java/com/mte/baseinfrastructure/MainActivity.kt
+++ app/src/main/java/com/mte/baseinfrastructure/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.mte.baseinfrastructure
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.ViewDataBinding
+import com.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ViewDataBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override val layoutRes: Int
+        get() = R.layout.activity_main
+
+    override fun initUI(savedInstanceState: Bundle?) {
+
     }
 }
