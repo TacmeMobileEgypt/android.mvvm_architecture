32d0a17fa8ba6d8e20d169fe0d6267d805a6b735
hsaad <hsaad@tacme.com>
2020-01-19 16:24:17 +0200

    enable databinding

--- app/src/main/java/com/mte/baseinfrastructure/MainActivity.kt
+++ app/src/main/java/com/mte/baseinfrastructure/MainActivity.kt
@@ -3,8 +3,9 @@ package com.mte.baseinfrastructure
 import android.os.Bundle
 import androidx.databinding.ViewDataBinding
 import com.base.BaseActivity
+import com.mte.baseinfrastructure.databinding.ActivityMainBinding
 
-class MainActivity : BaseActivity<ViewDataBinding>() {
+class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
